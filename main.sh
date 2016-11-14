3a0f724a86ca3f9de209d2061c02c8377be80e14
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2016-11-14 11:44:26 +0100

    Make box smaller (2x2x2)

--- main.sh
+++ main.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Create box with box lengths 6 x 6 x 6 nm^3
-gmx solvate -box 6 6 6 -o water_neutral.gro
+git clean -f
+
+# Create box with box lengths 2 x 2 x 2 nm^3
+gmx solvate -box 2 2 2 -o water_neutral.gro
 
 # Create topology file using the Amber force field
 gmx pdb2gmx -f water_neutral.gro -o water_neutral_processed.gro -water spce -ff "amber03"
