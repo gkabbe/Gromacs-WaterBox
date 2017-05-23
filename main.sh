8b57361643d155ca42a3e24b0b555fd410d164d2
kabbe <gabriel.kabbe@chemie.uni-halle.de>
2017-05-23 13:14:56 +0200

    Update main

--- main.sh
+++ main.sh
@@ -2,41 +2,37 @@
 
 git clean -f
 
-# Create box with box lengths 4 x 4 x 4 nm^3
-gmx solvate -box 2 2 2 -o water_neutral.gro
-
-gmx editconf -f water_neutral.gro -bt cubic -box 1.862017 -o water_neutral.gro
+# Create box with box lengths 2x2x2 nm^3
+gmx solvate -cs -box 2 2 2 -o water_neutral.gro
 
 # Create topology file using the Amber force field
 gmx pdb2gmx -f water_neutral.gro -o water_neutral_processed.gro -water tip4p -ff "amber03"
 
 # Create a run file for the energy minimization
-gmx grompp -f minim.mdp -c water_neutral_processed.gro -p topol.top -o energy_minimization.tpr
+gmx grompp -f minim.mdp -c water_neutral_processed.gro -p topol.top -o input_energy_minimization.tpr
 
 # Start the energy minimization
-gmx mdrun -v -deffnm energy_minimization -o ener_min
+gmx mdrun -v -s input_energy_minimization.tpr -deffnm energy_minimization
 
 # Create a run file for the equilibration
-gmx grompp -v -f equilibration.mdp -c energy_minimization.gro -p topol.top -o nvt.tpr
+gmx grompp -v -f equilibration.mdp -c energy_minimization.gro -p topol.top -o input_nvt.tpr
 
 # Start the equilibration
-gmx mdrun -v -deffnm nvt
+gmx mdrun -v -s input_nvt.tpr -deffnm nvt
 
-
-# The following commands are optional, as the density is already close to the experimental value
 
 # Create run file for equilibration with pressure coupling
-# gmx grompp -v -f pressure_coupling.mdp -c nvt.gro -t nvt.cpt -p topol.top -o npt.tpr
+gmx grompp -v -f pressure_coupling.mdp -c nvt.gro -t nvt.cpt -p topol.top -o input_npt.tpr
 
 # Optimize box size
-# gmx mdrun -v -deffnm npt
+gmx mdrun -v -s input_npt.tpr -deffnm npt
 
 # Create run file for production run
-gmx grompp -v -f production_run.mdp -c nvt.gro -t nvt.cpt -p topol.top -o production.tpr
+gmx grompp -v -f production_run.mdp -c npt.gro -t npt.cpt -p topol.top -o input_production.tpr
 
 
 # Start production run
-gmx mdrun -v -deffnm production
+gmx mdrun -v -s input_production.tpr -deffnm production
 
 
 # Convert trajectory to pdb
